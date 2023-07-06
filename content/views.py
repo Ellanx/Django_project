a67ece51aeb5dc9c120203b5ff0606e50514f1ab
Im dong seong <ririro238@gmail.com>
2023-07-06 17:33:49 +0900

    main.html(modal basic setting)

--- content/views.py
+++ content/views.py
@@ -4,9 +4,6 @@ from .models import Feed
 
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all() # select * from content_feed가 됨 
-        return render(request, "main/main.html")
+        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed가 됨, '-id'는 id가 입력될시 역순으로 나옴      
+        return render(request, "main/main.html", context=dict(feed_list=feed_list))
 
-    def post(self, request):
-        print("포스트로 호출")
-        return render(request, "main/main.html")
