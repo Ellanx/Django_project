0329d92fcc46aa9326d7c22542070950a6041a33
Im dong seong <ririro238@gmail.com>
2023-07-07 23:51:00 +0900

    templates setting

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import Sub
 
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("main", Sub.as_view()),
     path("", Sub.as_view()),
+    path("user/", include("user.urls")),
 ]
